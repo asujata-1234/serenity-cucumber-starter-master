5e13358c6027c748b5479f1d9659866e2142f221
Sujata Avirneni <s_avirneni@gmail.com>
2019-12-03 11:09:15 -0500

    first commit

---
+++ src/test/java/starter/search/SearchForm.java
@@ -0,0 +1,13 @@
+package starter.search;
+
+import org.openqa.selenium.By;
+
+class SearchForm {
+    static By SEARCH_FIELD = By.cssSelector("#search_form_input_homepage");
+    static By SEARCH_BUTTON = By.cssSelector("#search_button_homepage");
+}
+
+
+//#search_form_input
+
+////*[@id="search_form_input"]
